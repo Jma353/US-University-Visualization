e8e86bf92a7ae223979151c8828f3f889b614856
Jma353 <jma353@cornell.edu>
2016-04-20 23:57:59 -0400

    Lat/long info in schools.json/pretty_schools.json

--- scripts/compose_json.py
+++ scripts/compose_json.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python 
 
+from lxml import html 
+import requests as r 
 import re # Regular expressions 
 import json
+import sys
 
-def compose_JSON(): 
+
+def get_lat_long(api_key, zip_code):
+
+	geocode = r.get("https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:" + zip_code + "&key=" + api_key)
+	location_json = geocode.json()["results"][0]["geometry"]["location"]
+	lat = location_json["lat"]
+	lng = location_json["lng"] 
+
+	return (lat, lng)
+
+
+def compose_JSON(api_key): 
 	# Obtain the scores.json file + corresponding JSON 
 	with open('../JSONS/scores.json') as score_JSON: 
 		score_json = json.load(score_JSON)
@@ -53,6 +67,20 @@ def compose_JSON():
 				admissions_JSON.pop("school", 0)
 				element.update(admissions_JSON)
 
+				# Get geographic info (lat/long)
+				zip_code = element["salary_info"]["zip_code"]
+
+				# Manually check for Cornell zipcode (b/c it's incorrect)
+				zip_code = "14850" if (zip_code == "14853") else zip_code
+				print zip_code
+
+				lat, lng = get_lat_long(api_key, zip_code)
+
+				# lat/long JSON addition 
+				location_JSON = { "location_info": { "lat": lat, "lng": lng } } 
+				element.update(location_JSON)
+
+
 				# Add the refined JSON 
 				final_json.append(element)
 
@@ -63,7 +91,7 @@ def compose_JSON():
 
 
 
-compose_JSON() 
+compose_JSON(str(sys.argv[1])) # Take in the api_key as an arg  
 
 
 
--- scripts/geocode_test.py
+++ scripts/geocode_test.py
@@ -4,12 +4,14 @@ from lxml import html
 import requests as r 
 import re # Regular expressions 
 import json
+import sys
+
 
 def geocode_test(api_key): 
 
-	geocode = r.get("https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:3755&key=" + api_key)
-	print geocode.json() # ["results"][0]["geometry"]["location"] 
+	geocode = r.get("https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:07733&key=" + api_key)
+	print geocode.json()["results"][0]["geometry"]["location"] 
 
 
+geocode_test(str(sys.argv[1]))
 
-geocode_test(api_key)
