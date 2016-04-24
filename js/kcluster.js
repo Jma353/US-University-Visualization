421324dd4ebbfbc8757b72f0f3b7f065f20d7820
Jma353 <jma353@cornell.edu>
2016-04-23 23:29:47 -0400

    Moving circle forward on mouseover, updates, + highlighting of proper circles on state mouseover

--- js/kcluster.js
+++ js/kcluster.js
@@ -3,10 +3,10 @@
 
 
 // Basic distance function 
-function distanceFrom (a, b, x, y) {
+function distanceFrom (a, b, xname, x, yname, y) {
 	return Math.sqrt(
-		(a[x] - b[x]) * (a[x] - b[x]) + 
-		(a[y] - b[y]) * (a[y] - b[y])
+		(a[xname][x] - b[xname][x]) * (a[xname][x] - b[xname][x]) + 
+		(a[yname][y] - b[yname][y]) * (a[yname][y] - b[yname][y])
 	); 
 }
 
@@ -30,11 +30,11 @@ function initalizeClusters (data, k) {
 // initialized to random points, return the data JSONs with an extra field 
 // `clusterId` indicating which cluster the values belong to (minDist away)
 // NOTE: returns a copy of the data (to disallow tampering w/original)
-function initializeData (data, clusters, x, y) {
+function initializeData (data, clusters, xname, x, yname, y) {
 	var dataCopies = []
 	for (var i = 0; i < data.length; i++) {
 		var distances = clusters.map(function(d) {
-			return distanceFrom(data[i], d, x, y); 
+			return distanceFrom(data[i], d, xname, x, yname, y); 
 		});
 		var minDist = d3.min(distances); 
 		var clusterIndex = distances.indexOf(minDist); 
@@ -53,15 +53,15 @@ function initializeData (data, clusters, x, y) {
 // the provided parameter values, and a series of clusters that need to be 
 // updated dependent on the current JSON data associated with them, change the 
 // cluster x + y values as necessary (mean of all associated data)
-function shiftClusters (data, clusters, x, y) {
+function shiftClusters (data, clusters, xname, x, yname, y) {
 
 	for (var i = 0; i < clusters.length; i++) {
 		// Finding relevant data 
 		var clusterId = clusters[i].id 
 		var relevantData = data.filter(function (d) { return d.clusterId == clusterId }); 
 		// Updating x + y values of cluster 
-		clusters[i][x] = d3.mean(relevantData, function (d) { return d[x] }); 
-		clusters[i][y] = d3.mean(relevantData, function (d) { return d[y] }); 
+		clusters[i][xname][x] = d3.mean(relevantData, function (d) { return d[xname][x] }); 
+		clusters[i][yname][y] = d3.mean(relevantData, function (d) { return d[yname][y] }); 
 	}	
 
 }
@@ -72,13 +72,13 @@ function shiftClusters (data, clusters, x, y) {
 // the provided parameter values, and a series of clusters that have been updated
 // to their proper locations, update the data's `clusterId` to be the closest 
 // cluster, and return the # of data values whose cluster changes 
-function changedClusters (data, clusters, x, y) {
+function changedClusters (data, clusters, xname, x, yname, y) {
 	// To track changed clusters 
 	var numChanged = 0; 
 
 	for (var i = 0; i < data.length; i++) {
 		var distances = clusters.map(function(d) {
-			return distanceFrom(data[i], d, x, y); 
+			return distanceFrom(data[i], d, xname, x, yname, y); 
 		});
 		var minDist = d3.min(distances); 
 		var cI = distances.indexOf(minDist); 
@@ -100,21 +100,25 @@ function changedClusters (data, clusters, x, y) {
 
 // Overall clustering function.  Takes in original data (`originalData`), # of 
 // clusters `k`, x and y namespaces, and a function to call after every clustering 
-// iteration.  Returns nothing.
-function kMeansCluster (originalData, k, x, y, iterFunc) {
+// iteration.  Returns data + clusters (to use in displaying the clustering). 
+function kMeansCluster (originalData, k, xname, x, yname, y) {
 	// Initialize 
-	var clusters = initalizeClusters(data, k); 
-	var data = initializeData(originalData, clusters, x, y); 
+	var clusters = initalizeClusters(originalData, k); 
+	var data = initializeData(originalData, clusters, xname, x, yname, y); 
 
 	// Initialize this to track the state of clustering
 	var numChanged = null; 
 	while (numChanged == null || numChanged > 0) {
 
-		shiftClusters(data, clusters, x, y); // Shift the clusters 
-		iterFunc(); 
-		numChanged = changedClusters(data, clusters, x, y); // Recalc 
+		shiftClusters(data, clusters, xname, x, yname, y); // Shift the clusters 
+		//iterFunc();  -- Would theoretically be a function run after each iteration 
+		numChanged = changedClusters(data, clusters, xname, x, yname, y); // Recalc 
 
 	}
+
+	// Returns data, clusters 
+	return [data, clusters]
+
 
 }
 
